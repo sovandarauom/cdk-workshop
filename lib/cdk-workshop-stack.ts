300635624cc0a126259f15cf87f3e2596dcbade9
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2021-07-11 11:54:40 +0700

    Implement Gateway & lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,23 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import { FunctionProps } from '@aws-cdk/aws-lambda';
+import * as gateway from '@aws-cdk/aws-apigateway'
+import { LambdaRestApiProps } from '@aws-cdk/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // defines an AWS Lambda resource
+    const properties: FunctionProps = {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    }
+    const hello = new lambda.Function(this, 'HelloHandler', properties);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const lambdaRestApiProps: LambdaRestApiProps = {handler : hello}
+    new gateway.LambdaRestApi(this, 'Endpoint', lambdaRestApiProps)
   }
 }
+``
